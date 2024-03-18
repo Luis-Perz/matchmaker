d7a893f373846b599cd300e5c4710e148836960d
Luis Perez <luis998.ap@gmail.com>
2024-03-17 22:42:21 -0500

    Updated JS and HTML to reflect user overall score

--- index.js
+++ index.js
@@ -1,67 +1,48 @@
-var ques1 = document.getElementById("question1").value;
-var ques2 = document.getElementById("question2").value;
-var ques3 = document.getElementById("question3").value;
-var ques4 = document.getElementById("question4").value;
-var ques5 = document.getElementById("question5").value;
+document.getElementById("resCont2").style.visibility = "hidden";
+function inputValidation(){
+    var userInput1 = parseInt(document.getElementById("question1").selectedOptions[0].value);
+    var userInput2 = parseInt(document.getElementById("question2").selectedOptions[0].value); 
+    var userInput3 = parseInt(document.getElementById("question3").selectedOptions[0].value); 
+    var userInput4 = parseInt(document.getElementById("question4").selectedOptions[0].value);
+    var userInput5 = parseInt(document.getElementById("question5").selectedOptions[0].value);
+    
+    var sumInputs = userInput1 + userInput2 + userInput3 + userInput4 + userInput5;
+    var sumTotal = 100 - sumInputs;
+
+    return sumTotal;
+ }
 
 
-function compatResults(){
-    var ques1 = document.getElementById("question1").value;
-    var ques2 = document.getElementById("question2").value;
-    var ques3 = document.getElementById("question3").value;
-    var ques4 = document.getElementById("question4").value;
-    var ques5 = document.getElementById("question5").value;
-    total = parseInt(ques1) + parseInt(ques2) + parseInt(ques3) + parseInt(ques4) + parseInt(ques5);
-    score = 100 - total;
-    return score;
-}
+function resultMessage(){
+    var userInput1 = parseInt(document.getElementById("question1").selectedOptions[0].value);
+    var userInput2 = parseInt(document.getElementById("question2").selectedOptions[0].value); 
+    var userInput3 = parseInt(document.getElementById("question3").selectedOptions[0].value); 
+    var userInput4 = parseInt(document.getElementById("question4").selectedOptions[0].value);
+    var userInput5 = parseInt(document.getElementById("question5").selectedOptions[0].value);
+    
+    var sumInputs = userInput1 + userInput2 + userInput3 + userInput4 + userInput5;
+    var sumTotal = 100 - sumInputs;
 
-function resultMsg(){
-    var user_Msg;
-    if (compatResults() > 80){
-        user_Msg = "Got any plans for this weekend?"
+    if (sumTotal >= 80){
+        document.getElementById("resultMsg").textContent = "Let's go to Chuck E. Cheese";
+    } else if (sumTotal >= 60){
+        document.getElementById("resultMsg").textContent = "I guess we can be friends";
+    } else{  
+        document.getElementById("resultMsg").textContent = "Tragic...";
     }
-    if (compatResults() > 60){
-        user_Msg = "You're cool, I guess we can be friends."
-    }
-    else (compatResults() < 60);{
-        user_Msg = "Go ahead and close this window. Try again."
-    }
-    return user_Msg;
 }
-
-
-function statementRes(){
-    var ques1 = document.getElementById("question1").value;
-    var ques2 = document.getElementById("question2").value;
-    var ques3 = document.getElementById("question3").value;
-    var ques4 = document.getElementById("question4").value;
-    var ques5 = document.getElementById("question5").value;
-    var que1Score = 100 - ques1;
-    var que2Score = 100 - ques2;
-    var que3Score = 100 - ques3;
-    var que4Score = 100 - ques4;
-    var que5Score = 100 - ques5;
-
-    document.getElementById("answer1").textContent = que1Score;
-    document.getElementById("answer2").textContent = que2Score;
-    document.getElementById("answer3").textContent = que3Score;
-    document.getElementById("answer4").textContent = que4Score;
-    document.getElementById("answer5").textContent = que5Score;
-}
-
 
 document.getElementById("redBtn").addEventListener("click", () => {
-    console.log(compatResults());
-    if (compatResults() <= -1){
+    
+    if (inputValidation() < 0 ){
         window.alert("Please select an option for all of the statements! Thank You.")
     }
     else{
-        window.open("./results.html");
+        document.getElementById("resCont2").style.visibility = "visible";
+        document.getElementById("question-list").style.visibility = "hidden";
+        console.log(inputValidation());
+        document.getElementById("resultPercent").textContent = inputValidation()+"%";
+        resultMessage();
     }
 
-    
-    statementRes();
 });
-
-document.getElementById("resultMsg").textContent ="hello";
